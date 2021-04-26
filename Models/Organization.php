e2a8e2c62302c0691669fdbadc12fda09a36228a
Vitaliy Fedulov <reamonn2008@mail.ru>
2021-04-26 12:07:28 +0300

    fix bag date

--- Models/Organization.php
+++ Models/Organization.php
@@ -9,12 +9,11 @@ class Organization extends Model
      * @param array $org
      * @param array $data
      * @param bool $is_open
-     * @param int $weekend
      */
     protected $data = [];
     public $org = [];
     public $is_open = null;
-    public $weekend = 5;
+    
 
 
     public function listOrganization(){
@@ -32,26 +31,29 @@ class Organization extends Model
      
         return $this->schedule($this->data);
     }
+    
+    
+
 
     public function schedule(array $data)
     {
          foreach($data as $key => $item){
+             
+            
 
-            if($this->day($item->day_of_week) != $this->weekend){
+            if($this->weekendDay($item->day_of_week)){
                 
-                
-                $this->org[$item->id]['name'] = $item->name;
-                $this->org[$item->id]['weekend'] =  $this->weekend();
-                $this->org[$item->id]['is_open'] = $this->is_open;       
-
-                     
-            }else{
-
                 $this->org[$item->id]['name'] = $item->name;
                 $this->org[$item->id]['weekdays'] = $this->weekdays($item->open,$item->close);
                 $this->org[$item->id]['schedule'] = substr($item->open,0,-3)."-".substr($item->close,0,-3);
                 $this->org[$item->id]['is_open'] = $this->is_open;  
-                   
+             
+                
+            }else{
+                $this->org[$item->id]['name'] = $item->name;
+                $this->org[$item->id]['weekend'] =  $this->weekend();
+                $this->org[$item->id]['is_open'] = $this->is_open;   
+            
             }
           
          } 
@@ -63,23 +65,8 @@ class Organization extends Model
      
      public function weekdays(string $open, string $close)
      {
-        // количество рабочих часов
-        $work_h = $this->workingHours($open,$close); 
-       
-        return  $work_h;
-     }
-
-
-     public function weekend()
-     {
-            return "Выходной день.";
-     }
-
-     public function workingHours(string $open, string $close)
-     {
-        
-        // количество часов в рабочем дне 
-        $work_time = $this->countWorkDay($open,$close);    
+       // график работы
+       $work_time = $this->workTime($open,$close);    
 
        
        if($work_time) {
@@ -90,20 +77,25 @@ class Organization extends Model
          
            return $this->beforeOpening($open);
        }
-    
+      
      }
-     
-     
-     public function countWorkDay($open,$close)
+
+
+     public function weekend()
      {
-       $cur_time = new DateTime();
+            return "Выходной день.";
+     }
+
+   
+     public function workTime($open,$close)
+     {
+       $cur_time = new \DateTime();
        $cur_time->format('H:i:s');
        
-       $time_to_begin = new DateTime($open);
-       $time_to_end = new DateTime($close);
+       $time_to_begin = new \DateTime($open);
+       $time_to_end = new \DateTime($close);
 
         if ($cur_time->getTimestamp() > $time_to_begin->getTimestamp() && $cur_time->getTimestamp() < $time_to_end->getTimestamp()){
-             
              return  $this->is_open = true;  
         }else{
              return  $this->is_open = false;  
@@ -126,29 +118,45 @@ class Organization extends Model
      {
           $cur_time = new \DateTime();
           $time_to_open = new \DateTime('tomorrow'.$open);    
-
           $time = $cur_time->diff($time_to_open);
 
           return $time->format('%h часа(ов) и %i минут до открытия');
      }
+     
+     
+     public function updateDate()
+     {
+         
+         $data = $this->listOrganization();
+         
+         $sql = "UPDATE schedule SET day_of_week=:day_of_week WHERE id=:id";
+         
+         $stmt= $pdo->prepare($sql);
+         $stmt->execute($data);
+         
+     }
 
 
 
-     public function day(array $day){
+     public function weekendDay($days){
 
-       $day = new \DateTime();
+        $day = new \DateTime();
+        
+   
+      
         
         if(array_search($day->format('N'), json_decode($days))+1)
         {
            return true; 
-            
+           
         }else{
             
            return false; 
            
         }
-     }
+        
 
+     }
 
 
 }  
